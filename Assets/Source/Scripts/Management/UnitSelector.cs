1dec8c834b0a962fc7aca80aaa32b63ede419fe5
Александр Комаров <anthropogen96@gmail.com>
2023-03-09 14:17:08 +0300

    unit selector

---
+++ Assets/Source/Scripts/Management/UnitSelector.cs
@@ -0,0 +1,60 @@
+using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelector : NetworkBehaviour
+{
+    [SerializeField] private LayerMask unitMask;
+    private readonly List<Unit> selectedUnits = new List<Unit>();
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if (!isOwned) return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            ClearSelectionUnits();
+        }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+    }
+
+    private void ClearSelectionUnits()
+    {
+        if (selectedUnits.Count == 0)
+            return;
+
+        foreach (var u in selectedUnits)
+            u.Unselect();
+
+        selectedUnits.Clear();
+    }
+
+    private void ClearSelectionArea()
+    {
+        var ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out var hit, 100, unitMask))
+        {
+            if (hit.collider.TryGetComponent(out Unit unit))
+            {
+                selectedUnits.Add(unit);
+            }
+        }
+        foreach (var u in selectedUnits)
+            u.Select();
+    }
+}
--- Assets/Source/Scripts/Unit/Unit.cs
+++ Assets/Source/Scripts/Unit/Unit.cs
@@ -1,7 +1,29 @@
+using Mirror;
 using RTS.Configs;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Unit : MonoBehaviour
+public class Unit : NetworkBehaviour
 {
     [field: SerializeField] public UnitType UnitType { get; private set; }
+    public UnityEvent Selected;
+    public UnityEvent Unselected;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!isOwned) return;
+        Selected?.Invoke();
+    }
+
+    [Client]
+    public void Unselect()
+    {
+        if (!isOwned) return;
+        Unselected?.Invoke();
+    }
+
+    #endregion
 }
